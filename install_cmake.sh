c8bdd88437d496ad405eb3a339eb155c06409fbc
Tarun Jangra <tarun@izap.in>
2022-07-08 02:35:01 -0700

    small fix

--- install_cmake.sh
+++ install_cmake.sh
@@ -1,7 +1,6 @@
 #!/bin/ash
 VERSION=3.22.5
 arch=$(uname -m);
-PACKAGE="cmake-${VERSION}-linux-${ARCH}.tar.gz";
 case "$arch" in 
     'amd64')
         ARCH="x86_64";
@@ -15,6 +14,7 @@ case "$arch" in
     *) echo >&2 "error: unsupported architecture: '$arch'"; exit 1 ;;
 esac;
 
+PACKAGE="cmake-${VERSION}-linux-${ARCH}.tar.gz";
 curl -L --progress-bar --insecure https://github.com/Kitware/CMake/releases/download/v${VERSION}/$PACKAGE --output /tmp/$PACKAGE
 tar xf /tmp/$PACKAGE --directory /tmp
 cd /tmp/$PACKAGE
