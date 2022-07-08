8189581e136ade7f70bfc1b2acd145cb00fb9d80
Tarun Jangra <tarun@izap.in>
2022-07-08 02:46:19 -0700

    fix for variables

--- install_cmake.sh
+++ install_cmake.sh
@@ -1,9 +1,9 @@
 #!/bin/ash
 VERSION=3.22.5
 PACKAGE="cmake-${VERSION}.tar.gz";
-curl -L --progress-bar --insecure https://github.com/Kitware/CMake/releases/download/v${VERSION}/${PACKAGE} --output /tmp/${PACKAGE}
+curl -L --progress-bar --insecure https://github.com/Kitware/CMake/releases/download/v${VERSION}/${PACKAGE} --output /tmp/${VERSION}
 tar xf /tmp/$PACKAGE --directory /tmp
-cd /tmp/${PACKAGE}
+cd /tmp/${VERSION}
 ./configure
 make
 make install
